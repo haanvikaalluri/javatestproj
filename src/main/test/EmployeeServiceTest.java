bf425a1820770155c459cebfbf06c61c52157bbe
haanvikaalluri <142106967+haanvikaalluri@users.noreply.github.com>
2024-03-12 11:26:45 +0530

    Update EmployeeServiceTest.java

--- src/main/test/EmployeeServiceTest.java
+++ src/main/test/EmployeeServiceTest.java
@@ -1,1 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+pubic class BasicTests{
+
+  @Test
+  public void TestingMycase(){
+    //Developer some logic
+    Assertions.assertEquals(1,1);
+  }
+  
+}
